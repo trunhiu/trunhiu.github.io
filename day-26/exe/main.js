1157041e8db45c793eefe4e15fae2592b2b2bdd0
trunhiu <buihieu3008@gmail.com>
2022-07-01 21:27:45 +0700

    update

--- day-26/exe/main.js
+++ day-26/exe/main.js
@@ -1,11 +1,13 @@
 $(".menu-icon").click(function () {
     $(".menu-item ul").css("left", "0");
     $(".overlay").css("display", "block");
+    $("body").css("overflow-y", "hidden");
 })
 
 $(".overlay").click(function () {
     $(".menu-item ul").css("left", "-250px");
     $(".overlay").css("display", "none");
+    $("body").css("overflow-y", "scroll");
 });
 
 $(window).resize(function () {
