deea3da55568b1d79b8d7a0650e0034657763478
trunhiu <buihieu3008@gmail.com>
2022-07-29 22:16:31 +0700

    UPDATE

---
+++ project-ffood/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $('#banner').slick({
+        isFinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: true,
+    });
+});
+
+$(document).ready(function () {
+    $('#feedback').slick({
+        isFinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: true,
+    });
+});
