3ced3b9dd3d5defcb84a6741f00028f11a82a2f0
trunhiu <buihieu3008@gmail.com>
2022-08-02 18:47:00 +0700

    update

--- project-ffood/js/chi-tiet-sp.js
+++ project-ffood/js/chi-tiet-sp.js
@@ -7,13 +7,89 @@ $(document).ready(function () {
         asNavFor: '.detail-slider-item',
     });
     $('.detail-slider-item').slick({
-        slidesToShow: 3,
+        slidesToShow: 4,
         asNavFor: '.detail-item',
         arrows: false,
         dots: false,
-        centerMode: true,
+
         focusOnSelect: true,
     });
 
 });
+
+$(document).ready(function () {
+    $('.detail-logo-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        isFinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true
+                }
+            }
+        ]
+    });
+});
+
+$(document).ready(function () {
+    $('.hotproduct-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    arrows: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    arrows: true
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    arrows: true
+                }
+            }
+        ]
+    });
+});
+
 
---
+++ project-ffood/js/data.js
@@ -0,0 +1,17 @@
+// Danh sach san pham
+
+let products = [
+    {
+        id: 1,
+        name: "sản phẩm 1",
+        images: [
+            "",
+            "https://images.unsplash.com/photo-1655579934784-d988195a1319?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDExfGhtZW52UWhVbXhNfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+            "https://images.unsplash.com/photo-1655579933212-250c7073e342?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDl8aG1lbnZRaFVteE18fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+            "https://images.unsplash.com/photo-1602166226613-260bab4048f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDEyfGhtZW52UWhVbXhNfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        ],
+        rating: 4.5,
+        price: 400000,
+        description: "Mô tả 1",
+    },
+];
