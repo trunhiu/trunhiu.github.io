2ea023e567aa4cce0620181a346e3122d090929d
trunhiu <buihieu3008@gmail.com>
2022-08-05 14:28:38 +0700

    upadte

---
+++ project-ffood/js/lien-he.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    $('.instagram-slider').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    arrows: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    arrows: true
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    arrows: true
+                }
+            }
+        ]
+    });
+});
