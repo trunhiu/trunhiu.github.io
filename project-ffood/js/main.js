3595ecb07671bb8d64efe739e1fac1c63cf71e0f
trunhiu <buihieu3008@gmail.com>
2022-08-08 17:17:18 +0700

    update

--- project-ffood/js/main.js
+++ project-ffood/js/main.js
@@ -161,3 +161,43 @@ $(document).ready(function () {
     });
 });
 
+
+/************************ShoppingCart*************************/
+$(".header-shoppingcart").click(function () {
+    $(".header-shopping-cart").css("right", "0");
+    $(".header-overlay").css("display", "block");
+})
+
+$(".header-overlay").click(function () {
+    $(".header-shopping-cart").css("right", "-420");
+    $(".header-overlay").css("display", "none");
+})
+
+$(".btn-cancel").click(function () {
+    $(".header-shopping-cart").css("right", "-420");
+    $(".header-overlay").css("display", "none");
+})
+
+
+/****************login*****************/
+$(".header-login").click(function () {
+    $(".login-overlay").css("opacity", "1");
+    $(".login-overlay").css("pointer-events", "all");
+    $(".login-content").css("opacity", "1");
+    $(".login-content").css("pointer-events", "all");
+    $(".login-content").css("top", "50px");
+})
+
+$(".login-overlay").click(function (e) {
+    $(".login-overlay").css("opacity", "0");
+    $(".login-overlay").css("pointer-events", "none");
+    $(".login-content").css("opacity", "0");
+    $(".login-content").css("pointer-events", "none");
+})
+
+$(".btn-login-cancel").click(function () {
+    $(".login-overlay").css("opacity", "0");
+    $(".login-overlay").css("pointer-events", "none");
+    $(".login-content").css("opacity", "0");
+    $(".login-content").css("pointer-events", "none");
+})
