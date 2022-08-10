bd54fb243ca938fc36695138be85be11732d91e7
trunhiu <buihieu3008@gmail.com>
2022-08-10 11:39:50 +0700

    update

---
+++ project-ffood/js/tin-tuc.js
@@ -0,0 +1,148 @@
+/************menu******************/
+$(".logo-menu").click(function () {
+    $(".menu-sidebar").css("left", "0");
+    $(".overlay").css("display", "block");
+})
+$(".overlay").click(function () {
+    $(".menu-sidebar").css("left", "-350");
+    $(".overlay").css("display", "none");
+    $(".menu-sidebar-mid").css("display", "block");  
+    $(".login-sidebar-mid").css("display", "none");
+    $(".register-sidebar-mid").css("display", "none");
+    $(".forgotpw-sidebar-mid").css("display", "none");
+    $(".menu-sidebar-top-list").css("color", "#fff");
+    $(".menu-sidebar-top-list").css("background-color", "#111111");
+    $(".menu-sidebar-top-login").css("color", "#111111");
+    $(".menu-sidebar-top-login").css("background-color", "#fff");
+})
+
+$(".btn-menu-cancel").click(function () {
+    $(".menu-sidebar").css("left", "-350");
+    $(".overlay").css("display", "none");
+    $(".menu-sidebar-mid").css("display", "block");
+    $(".login-sidebar-mid").css("display", "none");
+    $(".register-sidebar-mid").css("display", "none");
+    $(".forgotpw-sidebar-mid").css("display", "none");
+    $(".menu-sidebar-top-list").css("color", "#fff");
+    $(".menu-sidebar-top-list").css("background-color", "#111111");
+    $(".menu-sidebar-top-login").css("color", "#111111");
+    $(".menu-sidebar-top-login").css("background-color", "#fff");
+})
+
+
+
+/************************ShoppingCart*************************/
+$(".header-shoppingcart").click(function () {
+    $(".header-shopping-cart").css("right", "0");
+    $(".header-overlay").css("display", "block");
+})
+
+$(".header-overlay").click(function () {
+    $(".header-shopping-cart").css("right", "-420");
+    $(".header-overlay").css("display", "none");
+})
+
+$(".btn-cancel").click(function () {
+    $(".header-shopping-cart").css("right", "-420");
+    $(".header-overlay").css("display", "none");
+})
+
+
+/****************login*****************/
+$(".header-login").click(function () {
+    $(".login-overlay").css("opacity", "1");
+    $(".login-overlay").css("pointer-events", "all");
+    $(".login-content").css("opacity", "1");
+    $(".login-content").css("pointer-events", "all");
+    $(".login-content").css("top", "50px");
+})
+
+$(".login-overlay").click(function () {
+    $(".login-overlay").css("opacity", "0");
+    $(".login-overlay").css("pointer-events", "none");
+    $(".login-content").css("opacity", "0");
+    $(".login-content").css("pointer-events", "none");
+    $(".login-content").css("top", "0px");
+    $(".login-page").css("display", "block");
+})
+
+$(".btn-login-cancel").click(function () {
+    $(".login-overlay").css("opacity", "0");
+    $(".login-overlay").css("pointer-events", "none");
+    $(".login-content").css("opacity", "0");
+    $(".login-content").css("pointer-events", "none");
+    $(".login-content").css("top", "0px");
+    $(".login-page").css("display", "block");
+})
+
+
+/****************register*****************/
+
+$(".goto-register").click(function () {
+    $(".login-page").css("display", "none");
+    $(".register-page").css("display", "block");
+})
+
+$(".btn-goback").click(function () {
+    $(".login-page").css("display", "block");
+    $(".register-page").css("display", "none");
+})
+
+/****************forgot-pw*****************/
+
+$(".forgot-pw").click(function () {
+    $(".login-page").css("display", "none");
+    $(".forgot-password").css("display", "block");
+})
+
+$(".btn-forgotpw-goback").click(function () {
+    $(".login-page").css("display", "block");
+    $(".forgot-password").css("display", "none");
+})
+
+/**********************login sidebar ****************/
+
+$(".menu-sidebar-top-login").click(function () {
+    $(".login-sidebar-mid").css("display", "block");
+    $(".menu-sidebar-mid").css("display", "none");
+    $(".menu-sidebar-top-list").css("color", "#111111");
+    $(".menu-sidebar-top-list").css("background-color", "#fff");
+    $(".menu-sidebar-top-login").css("color", "#fff");
+    $(".menu-sidebar-top-login").css("background-color", "#111111");
+})
+
+$(".menu-sidebar-top-list").click(function () {
+    $(".login-sidebar-mid").css("display", "none");
+    $(".register-sidebar-mid").css("display", "none");
+    $(".forgotpw-sidebar-mid").css("display", "none");
+    $(".menu-sidebar-mid").css("display", "block");
+    $(".menu-sidebar-top-login").css("color", "#111111");
+    $(".menu-sidebar-top-login").css("background-color", "#fff");
+    $(".menu-sidebar-top-list").css("color", "#fff");
+    $(".menu-sidebar-top-list").css("background-color", "#111111");
+})
+
+
+/****************register sidebar*****************/
+
+$(".login-sidebar-mid-register").click(function () {
+    $(".login-sidebar-mid").css("display", "none");
+    $(".register-sidebar-mid").css("display", "block");
+})
+
+$(".register-sidebar-mid-register").click(function () {
+    $(".login-sidebar-mid").css("display", "block");
+    $(".register-sidebar-mid").css("display", "none");
+})
+
+/****************forgot-pw sidebar*****************/
+
+$(".login-sidebar-mid-forgot").click(function () {
+    $(".login-sidebar-mid").css("display", "none");
+    $(".forgotpw-sidebar-mid").css("display", "block");
+})
+
+$(".forgotpw-sidebar-mid-register").click(function () {
+    $(".login-sidebar-mid").css("display", "block");
+    $(".forgotpw-sidebar-mid").css("display", "none");
+})
